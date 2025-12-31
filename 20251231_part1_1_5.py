69fc4c1755fe07dd578a2adcd051f61341519611
Plank <michael2@Mac.fritz.box>
2025-12-31 13:08:36 +0100

    First commit

---
+++ 20251231_part1_1_5.py
@@ -0,0 +1,6 @@
+#1. Please write a program which asks the user for an integer number. The program should print out 
+ #"Orwell" if the number is exactly 1984, and otherwise do nothing.
+print('Please type a number: ')
+#### Don't know how to ask user for input...
+if input == 1984:
+    print('Orwell') 
